c9ae0e3877c8ed7a3ed0384f1edf94afb65da97a
kimjuah11-create <kimjuah4725@gmail.com>
2025-11-26 05:28:20 +0000

    최종 제출

--- 3_소스코드/main.c
+++ 3_소스코드/main.c
@@ -1,30 +1,4 @@
-#include <stdio.h>
 
-/*
-    1. [설계]에서 정의한 변수, 배열, (필요하면) 구조체를 여기에 선언하세요.
-    
-    예시 - 배열 사용:
-    char names[100][20];
-    int scores[100];
-    int count = 0;
-    
-    예시 - 구조체 사용 (선택):
-    struct Student {
-        char name[20];
-        int score;
-    };
-    struct Student students[100];
-*/
-
-
-/*
-    2. [알고리즘]에서 설계한 핵심 기능 함수들을 여기에 선언하세요.
-*/
-
-
-int main() {
-    
-    printf("--- C언어 미니 프로젝트 시작! ---\n");
 #include <stdio.h>
 #include <string.h>
 
